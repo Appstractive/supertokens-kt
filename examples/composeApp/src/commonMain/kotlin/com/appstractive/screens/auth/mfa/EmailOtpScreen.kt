2cd58c3aef39834b79239273f8242081c645a413
Andreas Schulz <andr.schulz90@gmail.com>
2024-05-03 16:30:40 +0200

    Add Email OTP Example

--- examples/composeApp/src/commonMain/kotlin/com/appstractive/screens/auth/mfa/EmailOtpScreen.kt
+++ examples/composeApp/src/commonMain/kotlin/com/appstractive/screens/auth/mfa/EmailOtpScreen.kt
@@ -1,21 +1,39 @@
 package com.appstractive.screens.auth.mfa
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import com.appstractive.LocalDependencies
+import com.appstractive.screens.home.HomeScreen
 import com.appstractive.util.CommonParcelize
+import com.appstractive.util.rememberApiCallController
 import com.slack.circuit.runtime.CircuitUiEvent
 import com.slack.circuit.runtime.CircuitUiState
 import com.slack.circuit.runtime.Navigator
 import com.slack.circuit.runtime.screen.Screen
 import com.supertokens.sdk.SuperTokensClient
+import com.supertokens.sdk.handlers.signInWith
+import com.supertokens.sdk.handlers.signUpWith
+import com.supertokens.sdk.recipes.passwordless.Passwordless
+import com.supertokens.sdk.recipes.passwordless.PasswordlessInputCode
+import com.supertokens.sdk.recipes.passwordless.PasswordlessSignUpData
 
 @CommonParcelize
 data object EmailOtpScreen : Screen {
     data class State(
+        val email: String?,
+        val emailSend: Boolean,
         val eventSink: (Event) -> Unit = {},
     ): CircuitUiState
 
     sealed interface Event : CircuitUiEvent {
+        data object SendEmail : Event
+        data class ConfirmCode(
+            val code: String,
+        ) : Event
         data object GoBack : Event
     }
 }
@@ -25,9 +43,30 @@ fun EmailOtpScreenPresenter(
     navigator: Navigator,
     superTokensClient: SuperTokensClient = LocalDependencies.current.superTokensClient,
 ): EmailOtpScreen.State {
-    return EmailOtpScreen.State {
+    val apiCallController = rememberApiCallController()
+    val claims by superTokensClient.userRepository.claims.collectAsState()
+    var signUpData by remember { mutableStateOf<PasswordlessSignUpData?>(null) }
+
+    return EmailOtpScreen.State(
+        email = claims?.email,
+        emailSend = signUpData != null,
+    ) {
         when (it) {
             EmailOtpScreen.Event.GoBack -> navigator.pop()
+            is EmailOtpScreen.Event.ConfirmCode -> apiCallController.call {
+                superTokensClient.signInWith(PasswordlessInputCode) {
+                    preAuthSessionId = signUpData?.preAuthSessionId
+                    deviceId = signUpData?.deviceId
+                    userInputCode = it.code
+                }
+
+                navigator.resetRoot(HomeScreen)
+            }
+            EmailOtpScreen.Event.SendEmail -> apiCallController.call {
+                signUpData = superTokensClient.signUpWith(Passwordless) {
+                    email = claims?.email
+                }
+            }
         }
     }
 }
--- examples/composeApp/src/commonMain/kotlin/com/appstractive/screens/auth/mfa/EmailOtpUI.kt
+++ examples/composeApp/src/commonMain/kotlin/com/appstractive/screens/auth/mfa/EmailOtpUI.kt
@@ -4,18 +4,28 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
+import com.slack.circuit.retained.rememberRetained
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -47,7 +57,42 @@ fun EmailOtpView(
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.spacedBy(8.dp, alignment = Alignment.CenterVertically)
         ) {
+            if (state.email != null) {
+                Text("Confirm Login with ${state.email}")
 
+                Button(
+                    onClick = {
+                        state.eventSink(EmailOtpScreen.Event.SendEmail)
+                    },
+                ) {
+                    Text(if (state.emailSend) "Resend OTP" else "Send OTP")
+                }
+
+                var otp by rememberRetained {
+                    mutableStateOf(TextFieldValue())
+                }
+
+                TextField(
+                    enabled = state.emailSend,
+                    label = { Text(text = "Code") },
+                    value = otp,
+                    onValueChange = { otp = it },
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Number,
+                    ),
+                )
+
+                Button(
+                    enabled = state.emailSend && otp.text.isNotEmpty(),
+                    onClick = {
+                        state.eventSink(EmailOtpScreen.Event.ConfirmCode(otp.text))
+                    },
+                ) {
+                    Text("Confirm")
+                }
+            } else {
+                CircularProgressIndicator()
+            }
         }
     }
 }
